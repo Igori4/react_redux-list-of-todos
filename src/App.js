b7e6b3d4c8c02eef6ab00a2d6860496c85e81bff
Igori4 <igorpilyak@gmail.com>
2019-07-15 21:21:21 +0300

    create increase and decrease

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+// import { createStore } from 'redux';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+// let store = createStore();
+
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  increes = () => {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  }
+
+  dencrees = () => {
+    this.setState(prevState => ({ count: prevState.count - 1 }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Count:
+          {this.state.count}
+        </h1>
+        <button type="button" onClick={this.increes}>+</button>
+        <button type="button" onClick={this.dencrees}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
